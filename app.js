8ff42c150486eeb040073856956503d391376b4a
sumit-jha94 <sumitshine14@gmail.com>
2024-01-08 03:43:13 +0530

    added images and other details

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-let api = 'http://www.omdbapi.com/?i=tt3896198&apikey=6cb42c23&t=rrr'
+let api = 'http://www.omdbapi.com/?i=tt3896198&apikey=6cb42c23&t='
 
-fetch(api).then(function(data){
-    return data.json();
-    
-}).then(function(data){
-    console.log(data);
-})
+
+
+function searchMovie() {
+    let query = document.getElementById('movieName').value;
+
+    // with search variable we got api link and search term from user and created a link
+    let search = api + query;
+    console.log(search)
+    fetch(search).then(function(data){
+        return data.json();
+        
+    }).then(function(data){
+        document.getElementById('title').innerText = data.Title;
+        document.getElementById('desc').innerText = data.Plot;
+        document.getElementById('actors').innerText = data.Actors;
+        document.getElementById('directors').innerText = data.Director;
+        document.getElementById('writers').innerText = data.Writer;
+        document.getElementById('collection').innerText = data.BoxOffice;
+        document.getElementById('awards').innerText = data.Awards;
+        document.getElementById('rating').innerText = data.imdbRating;
+        // we want to get the image in src of image
+        document.getElementById('poster').src = data.Poster;
+        console.log(data)
+       
+    })
+}
